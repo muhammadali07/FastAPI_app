b172a75d40cd108f7313d65513bb0f41a5d4e665
Muh Ali Bakhtiar <36842581+muhammadali07@users.noreply.github.com>
2021-04-16 13:06:43 +0700

    updating transaksi root

--- Login/app/main.py
+++ Login/app/main.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 import dns
 from routes.pulsa import router as PulsaRouter
 from routes.login import router as LoginRouter
+from routes.transaksi import router as TransaksiRouter
 
 app = FastAPI()
 
 app.include_router(LoginRouter, tags=["Login"], prefix="/login")
 app.include_router(PulsaRouter, tags=["Pulsa"], prefix="/pulsa")
+app.include_router(TransaksiRouter, tags=["Transaksi"], prefix="/transaksi")
 
 if __name__ == "__main__":
     uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
--- Login/app/routes/transaksi.py
+++ Login/app/routes/transaksi.py
@@ -1,8 +1,18 @@
 from fastapi import APIRouter
+from models.models_pulsa import *
 import connection
 
 router = APIRouter()
 
-router.post("/jual_pulsa/{nama_provider}/{nominal}", tags=["Transaksi"])
-def jual_pulsa(nama_provider, nominal:int):
-    return ""
+@router.post("/jual_pulsa/{kode_provider}/{qty_pulsa}", tags=["Transaksi"])
+def jual_pulsa(kode_provider:str, qty_pulsa:int):
+    try:
+        if connection.db.master_pulsa.find_one({"kode_provider": kode_provider}):
+            nama_provider = connection.db.master_pulsa.find_one({"kode_provider":kode_provider})
+            print(nama_provider)
+            connection.db.transaksi_pulsa.insert_one({"kode_provider":kode_provider, "nama_provider":nama_provider, "qty_pulsa":qty_pulsa})
+            return {"message":f"transaksi pulsa provider {kode_provider} berhasil"}
+        else:
+            return {"message":f"Kode Provider {kode_provider} tidak tersedia"}
+    except Exception:
+        return jual_pulsa
